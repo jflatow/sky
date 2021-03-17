fb23df170c1e074fb3d528a4292dca5ccd2ee8fc
Jared Flatow <jflatow@gmail.com>
2021-03-16 20:53:14 -0700

    Use jsdom in node

--- env/index.js
+++ env/index.js
@@ -1,24 +1,8 @@
 // create non-hostile environment within node
 
+const {JSDOM} = require("jsdom");
+
+({window} = new JSDOM(`<!DOCTYPE html>`));
+({document} = window);
+
 console.debug = console.log;
-
-window = {}
-window.document = document = {
-  createElementNS: function () {}
-}
-window.location = document.location = {
-  hash: '',
-  href: '',
-  search: ''
-}
-
-window.localStorage = localStorage = {}
-window.sessionStorage = sessionStorage = {}
-
-window.atob = atob = function atob(str) {
-  return new Buffer(str, 'base64').toString('utf8')
-}
-
-window.btoa = btoa = function btoa(str) {
-  return new Buffer(str).toString('base64')
-}
